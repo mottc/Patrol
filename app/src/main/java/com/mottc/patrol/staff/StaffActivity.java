46aa5e6cd619b306e75cf191c313980d67e8c512
mottc <mottcode@163.com>
2017-04-10 22:14:54 +0800

    添加staff底部导航栏

--- app/src/main/java/com/mottc/patrol/staff/StaffActivity.java
+++ app/src/main/java/com/mottc/patrol/staff/StaffActivity.java
@@ -1,15 +1,39 @@
 package com.mottc.patrol.staff;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.mottc.patrol.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class StaffActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.bnv_menu)
+    BottomNavigationView mBnvMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff);
+        ButterKnife.bind(this);
+        mToolbar.setTitle("巡更");
+        setSupportActionBar(mToolbar);
+        mBnvMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                }
+                return true;
+            }
+        });
     }
 }
