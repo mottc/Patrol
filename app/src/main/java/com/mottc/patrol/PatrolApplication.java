6bf4a3dae5c350e947bac73885a4ba0ebc008c3a
mottc <mottcode@163.com>
2017-04-13 14:39:13 +0800

    add Application

---
+++ app/src/main/java/com/mottc/patrol/PatrolApplication.java
@@ -0,0 +1,13 @@
+package com.mottc.patrol;
+
+import android.app.Application;
+
+/**
+ * Created with Android Studio
+ * User: mottc
+ * Date: 2017/4/13
+ * Time: 14:38
+ */
+public class PatrolApplication extends Application {
+
+}
